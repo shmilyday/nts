d1ebd23b4688dbda31be0a8c3f7664ada95e7c7f
lonelyclick <lonelyclick@163.com>
2014-09-29 18:07:10 +0800

    面试题搜寻

---
+++ blog/mianshi.js
@@ -0,0 +1,44 @@
+/**
+ * 5个经典的前端面试问题 http://www.admin10000.com/document/5233.html
+ * 
+ *  ① web前端优化
+ *  	从减少http请求数说起 http://www.cnblogs.com/fsjohnhuang/articles/2422787.html
+ *  	Web前端优化 http://www.cnblogs.com/wildweeds/archive/2010/06/12/web_performance.html
+ *  	浏览器允许的并发请求资源数是什么意思？ http://www.zhihu.com/question/20474326/answer/15696641
+ *  	浏览器并发连接数(转载） http://www.cnblogs.com/flyingchen/archive/2009/09/03/1559945.html
+ *  	详解浏览器最大并发连接数 http://www.iefans.net/liulanqi-zuida-bingfa-lianjieshu/
+ *  	HTML页面元素加载顺序研究报告（1）----简单Div罗列 http://www.cnblogs.com/smjack/archive/2008/08/20/1272370.html
+ *  	HTML页面元素加载顺序研究报告（2）----背景图片 http://www.cnblogs.com/smjack/archive/2008/08/21/1272905.html
+ *  	HTML页面元素加载顺序研究报告（3）----Script标签与背景图片 http://www.cnblogs.com/smjack/archive/2008/08/25/1276082.html
+ *      HTML页面元素加载顺序研究报告（4）----用JavaScript插入图片 http://www.cnblogs.com/smjack/archive/2009/03/02/1279779.html
+ *      HTML页面元素加载顺序研究报告（5）----Script标签在Firefox下对背景图片的影响 http://www.cnblogs.com/smjack/archive/2009/03/02/1279787.html
+	② 事件冒泡（选项卡的实现）
+	③ CSS布局相关
+	④ 应该用float吗？
+	⑤ 模块化编程
+	⑥ 为什么有闭包
+	⑦ 延迟请求
+	
+	语义：
+		http://www.zhangxinxu.com/wordpress/2011/11/i-b-em-strong-html5-%E8%AF%AD%E4%B9%89/
+		http://www.wzsky.net/html/Website/htmlcss/103039.html
+		http://www.nowamagic.net/librarys/veda/detail/506
+	HTML
+		http://club.topsage.com/thread-2738399-1-1.html
+		你所未必知道的关于<!DOCTYPE>标签细节 http://www.nowamagic.net/librarys/veda/detail/120
+		关于浏览器渲染html页面采用的三种文本模式 http://nirvana1988.iteye.com/blog/1471578
+		HTML文档类型DTD与浏览器怪异模式 http://blog.csdn.net/freshlover/article/details/11616563
+		浏览器兼容性问题与浏览器的内核及渲染模式 http://www.cnblogs.com/lhb25/archive/2010/09/18/1829849.html
+		使用X-UA-Compatible来设置IE浏览器兼容模式 http://www.cnblogs.com/nidilzhang/archive/2010/01/09/1642887.html
+		IE8的兼容渲染模式X-UA-Compatible http://www.nowamagic.net/librarys/veda/detail/925
+	CSS
+		收集Web前端 面试题 HTML+CSS部分 http://www.cnblogs.com/leejersey/archive/2012/11/01/2749352.html
+		CSS面试题——不断补充 http://bbs.miaov.com/forum.php?mod=viewthread&tid=976
+		
+	兼容性
+		IE6-IE9兼容性问题列表及解决办法总结 http://www.cnblogs.com/liuzhendong/archive/2012/04/09/2438502.html
+		IE8的兼容渲染模式X-UA-Compatible http://www.nowamagic.net/librarys/veda/detail/925
+ * 
+ */
+
+
