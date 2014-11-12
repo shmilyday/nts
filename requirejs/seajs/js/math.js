6a74884515377e12368f78ac021bdfd1157bdf0c
lonelyclick <lonelyclick@163.com>
2014-11-12 19:24:19 +0800

    seajs和hash命令空间类库

---
+++ requirejs/seajs/js/math.js
@@ -0,0 +1,13 @@
+/**
+ * Created by yangluguang on 2014/11/12.
+ */
+define(function (require, exports, module) {
+    module.exports = {
+        add: function (a, b) {
+            return a + b;
+        },
+        say: function () {
+            return 'hello';
+        }
+    };
+});
